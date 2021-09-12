a18f1211feee45ec8e1e9f59e6c7f0f834efbdcf
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-09-12 23:01:39 +0200

    Add stuff for testing

--- src/main/java/nl/codevs/decree/decrees/MainCommandClass.java
+++ src/main/java/nl/codevs/decree/decrees/MainCommandClass.java
@@ -2,15 +2,23 @@ package nl.codevs.decree.decrees;
 
 import nl.codevs.decree.decree.objects.DecreeCommandExecutor;
 import nl.codevs.decree.decree.objects.Decree;
+import nl.codevs.decree.decree.objects.DecreeOrigin;
 
 @SuppressWarnings("SpellCheckingInspection")
-@Decree(name = "command", aliases = {"cmmd", "cmd", "cd"}, description = "Main commands", permission = "decree")
+@Decree(name = "command", aliases = {"cmmd", "cmd", "cd"}, description = "Main commands", permission = "decree", origin = DecreeOrigin.PLAYER)
 public class MainCommandClass implements DecreeCommandExecutor {
 
     // This line is a category pointer
     private SubCommandClass nameDoesNotMatterHere;
 
-    @Decree
+    @Decree(
+            description = "Description goes here",
+            aliases = {"alias1", "alias2"},
+            sync = true,
+            origin = DecreeOrigin.PLAYER,
+            permission = "decree.hello",
+            name = "command123"
+    )
     public void hello(){
         sender().sendMessage("Hey!");
     }
