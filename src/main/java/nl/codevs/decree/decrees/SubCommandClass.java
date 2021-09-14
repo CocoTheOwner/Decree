3ee6626bf26f14883135767fddd40cfcb5d2faf1
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-09-14 19:41:10 +0200

    Better testing

--- src/main/java/nl/codevs/decree/decrees/SubCommandClass.java
+++ src/main/java/nl/codevs/decree/decrees/SubCommandClass.java
@@ -3,6 +3,7 @@ package nl.codevs.decree.decrees;
 import nl.codevs.decree.decree.objects.DecreeCommandExecutor;
 import nl.codevs.decree.decree.objects.DecreeOrigin;
 import nl.codevs.decree.decree.objects.Decree;
+import nl.codevs.decree.decree.objects.Param;
 
 @Decree(name = "sub", aliases = {"0", "1", "2", "3", "five"}, description = "Sub commands", permission = "subs")
 public class SubCommandClass implements DecreeCommandExecutor {
@@ -14,7 +15,14 @@ public class SubCommandClass implements DecreeCommandExecutor {
             permission = "kill",
             sync = true
     )
-    public void hello(){
-        sender().sendMessageRaw("yeet");
+    public void hello(
+            @Param(
+                    name = "Name",
+                    aliases = {"1", "16"},
+                    description = "Idk",
+                    defaultValue = "Text!"
+            ) String message
+    ){
+        sender().sendMessageRaw(message);
     }
 }
